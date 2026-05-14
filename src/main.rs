afd68aab19471cb4a83d213f39435fef3167363a
Ozonichi <ozonichi@gmail.com>
2026-05-14 23:07:06 +0100

    feat: add TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::TcpListener;
+
+const ADDR: &str = "127.0.0.1:6821";
+const MSG: u32 = 32;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(ADDR).expect("Listener failed to start");
+    listener
+        .set_nonblocking(true)
+        .expect("Error setting serer to non-blocking");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Connection Established");
+                println!("{:?}", stream)
+            }
+            Err(_e) => {}
+        }
+    }
 }
